31141c6eb3d3d6ce215bbc6f076d1277dcacf39f
GeraldusCiterTeja <duateja@gmail.com>
2024-06-09 23:06:17 +0700

    menambahkan fitur pada css

--- style.js
+++ style.js
@@ -1,3 +1,31 @@
+// project 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const project = document.querySelectorAll('.project-container');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Optional: stop observing once it's visible
+            }
+        });
+    }, observerOptions);
+
+    project.forEach(project => {
+        observer.observe(project);
+    });
+});
+
+
+// certificate 
+
 document.addEventListener('DOMContentLoaded', function() {
     const certificates = document.querySelectorAll('.certificate-container');
 
